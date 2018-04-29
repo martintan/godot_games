0cda351d7ff629672cedb2da8b37e1c8c84e9225
martintan <31561845+martintan@users.noreply.github.com>
2018-04-30 06:41:16 +0800

    Add files via upload

--- Procedural Miner/Blocks/BlockFactory.gd
+++ Procedural Miner/Blocks/BlockFactory.gd
@@ -1,11 +1,18 @@
 extends Node
 
+export var block_queue_size = 5
+# queue structure for blocks
+# push front, pop back
+var block_queue = []
 
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	randomize()
+	for i in block_queue_size:
+		block_queue.push_front( get_random_block() )
+	
+func get_random_block():
+	return get_child(rand_range(0, get_child_count())).duplicate()
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
